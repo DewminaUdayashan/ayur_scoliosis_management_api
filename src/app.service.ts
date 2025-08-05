375a7f597e6ced0fa226787574addb0be097c977
Dewmina Udayashan <dewminaudayashan@gmail.com>
2025-08-05 22:49:59 +0530

    feat: update health check endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'The service is up and running!';
+  health(): { message: string; version: string; description: string } {
+    return {
+      message: 'Welcome to the Ayur Scoliosis Management API!',
+      version: '1.0.0',
+      description:
+        'This API provides functionalities for managing scoliosis patients, including inviting patients, managing appointments, and more.',
+    };
   }
 }
