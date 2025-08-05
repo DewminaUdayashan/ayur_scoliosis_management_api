cef6feff2d3f9223b02ef3414b8d06c140e4f94a
Dewmina Udayashan <dewminaudayashan@gmail.com>
2025-08-05 21:10:34 +0530

    feat(auth): reset password

---
+++ prisma/migrations/20250805151120_add_password_reset_table/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "public"."PasswordReset" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "PasswordReset_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "PasswordReset_token_key" ON "public"."PasswordReset"("token");
+
+-- CreateIndex
+CREATE INDEX "PasswordReset_email_token_idx" ON "public"."PasswordReset"("email", "token");
